a3b560a5121d8fd7a6cc9cc5a91f5ca36e214070
El mono Linuxero <root@localhost.localdomain>
2018-02-15 17:54:57 +0100

    Subido ejercicio_14

--- ejercicio_14.py
+++ ejercicio_14.py
@@ -1,3 +1,8 @@
 import paquetes.ficheros
+import sys
+#print paquetes.ficheros.creadir('/tmp/prueba')
 
-print paquetes.ficheros.creadir('/tmp/prueba')
+if len(sys.argv) <> 2:
+	print "Funcion de programa: ejercicio_14.py <directorio_a_crear>"
+else:
+	print paquetes.ficheros.creadir(sys.argv[1])
